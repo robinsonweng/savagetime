3cd4e40c98f0e4b72873bac66d342552634f750f
robinsonweng <robinsonweng100@gmail.com>
2021-09-17 16:11:19 +0800

    Add homepage url Adding url for homepage in urls.py

--- savagetime/savagetime/urls.py
+++ savagetime/savagetime/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from home.views import index
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', index),
 ]
