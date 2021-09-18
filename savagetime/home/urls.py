9ede24bec6cbbdc4c5454458db6583cb402519ba
robinsonweng <robinsonweng100@gmail.com>
2021-09-18 16:32:41 +0800

    Add media url for debug build

--- savagetime/home/urls.py
+++ savagetime/home/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
@@ -7,4 +8,4 @@ urlpatterns = [
     path('series/<int:series_id>/', views.series, name='series'),
     path('video/<int:video_id>/', views.video, name='video'),
     path('search/<str:query>/', views.search, name='search'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
