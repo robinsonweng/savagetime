b0affbc823e0a1d4530fb6c3b238c9290bda683d
robinsonweng <robinsonweng100@gmail.com>
2021-09-18 18:29:55 +0800

    Add media/static url for debug

--- savagetime/home/urls.py
+++ savagetime/home/urls.py
@@ -8,4 +8,9 @@ urlpatterns = [
     path('series/<int:series_id>/', views.series, name='series'),
     path('video/<int:video_id>/', views.video, name='video'),
     path('search/<str:query>/', views.search, name='search'),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    print(urlpatterns)
