0fe8dffc99d6a59978a8a138294a4795a9631703
robinsonweng <robinsonweng100@gmail.com>
2021-09-19 18:01:25 +0800

    Make indexing for route search/

--- savagetime/home/views.py
+++ savagetime/home/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 def index(request):
     return HttpResponse("this is our home")
@@ -8,10 +9,13 @@ def series(request, series_id):
     return HttpResponse("select series here")
 
 def video(request, video_id=0):
-    return HttpResponse("shows pecific video here")
+    temp = loader.get_template('view_video.html')
+    context = {}
+    return HttpResponse(temp.render(context, request))
 
-def search(request, query):
-    return HttpResponse("return search result")
+def search(request):
+    keyword = request.GET.get('s')
+    return HttpResponse(f"you search for {keyword}")
 
 def error(request, query):
     return HttpResponse("404 yee")
