7e3f4e0da895abeed7231eed084fa46c978ab236
robinsonweng <robinsonweng100@gmail.com>
2021-10-23 13:40:06 +0800

    Register db models to app

--- savagetime/home/admin.py
+++ savagetime/home/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from home import models
 # Register your models here.
+
+admin.site.register(models.Season)
+admin.site.register(models.Series)
+admin.site.register(models.Video)
