574bd41f247c804ee65078adb3625bd76d535249
robinsonweng <robinsonweng100@gmail.com>
2021-10-25 13:11:00 +0800

    Change DB from sqlite to postgresql

--- savagetime/savagetime/settings.py
+++ savagetime/savagetime/settings.py
@@ -89,8 +89,10 @@ WSGI_APPLICATION = 'savagetime.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'anime',
+        'HOST': '',
+        "USER": 'robinson'
     }
 }
 
