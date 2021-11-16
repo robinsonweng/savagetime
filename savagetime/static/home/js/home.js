dc24bebfb4d6713ae528c8eb54559747bf99a7a7
robinsonweng <robinsonweng100@gmail.com>
2021-11-16 18:32:11 +0800

    Make table filter work on flex box

--- savagetime/static/home/js/home.js
+++ savagetime/static/home/js/home.js
@@ -1,34 +1,37 @@
 // init not found message
 let text = document.createTextNode("Keyword not found");
-let td = document.createElement("td");
-td.appendChild(text);
-message = document.createElement("tr");
-message.appendChild(td);
-message.style.display = "none";
-document.getElementsByTagName("tbody")[0].appendChild(message);
+let para = document.createElement("p");
+para.appendChild(text);
+let block = document.createElement("div");
+block.appendChild(para);
+let li = document.createElement("li");
+li.appendChild(block);
+li.className = "table-row not-found";
+li.style.display = "none";
+document.getElementById("main-table").appendChild(li);
 
 let input = document.getElementById("table-filter");
 key_listener = (e)=> {
     let table = document.getElementById("main-table");
-    let a_tag = table.querySelectorAll(".table-body tr td a");
-    let keyword = e.target.value;
-
+    let row = table.getElementsByClassName("table-row");
     // iterate all series name
     let counter = 0;
-    for (i = 0; i < a_tag.length; i ++){
-        s_result = a_tag[i].textContent.indexOf(keyword);
-        if (s_result > -1){
-            a_tag[i].closest("tr").style.display = "";
-            message.style.display = "none";
+    for (i = 0; i < row.length; i ++){
+        title_text = row[i].querySelectorAll("div")[0].firstChild.innerText;
+        console.log(input.innerText)
+        result = title_text.indexOf(input.value)
+        if (result > -1){
+            row[i].style.display = "";
+            li.style.display = "none"
         } else {
             counter += 1;
-            a_tag[i].closest("tr").style.display = "none";
+            row[i].style.display = "none";
         }
-    }
 
-    if (counter == a_tag.length) {
+    }
+    if (counter == row.length) {
         // if loop set every display to none, it means no keyword match
-        message.style.display = "";
+        li.style.display = "";
     }
 }
 input.oninput = key_listener;
