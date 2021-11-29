f08f798b4c4be9eeeaea85e2e9b3db7fdf8d7414
robinsonweng <robinsonweng100@gmail.com>
2021-11-29 18:57:19 +0800

    Use regex to filter wrong url

--- savagetime/home/urls.py
+++ savagetime/home/urls.py
@@ -6,8 +6,11 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('series/<int:series_id>/', views.series, name='series'),
-    path('video/<int:video_id>/', views.video, name='video'),
+    path('series/<str:series_id>/', views.series, name='series'),
+    re_path(
+        r'^watch/(?P<series_id>[0-9a-f]{8}-[0-9a-f]{4}-[4][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12})$',
+        views.video, name='video',
+    ),
     re_path(r'^search/$', views.search, name='search'),
 ]
 
