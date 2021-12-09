f538d3253ec5d99298c1f65f29048136c4494c54
robinsonweng <robinsonweng100@gmail.com>
2021-12-09 14:45:01 +0800

    Add basic structure for api

--- savagetime/savagetime/api_dev.py
+++ savagetime/savagetime/api_dev.py
@@ -3,6 +3,21 @@ from ninja import NinjaAPI
 api = NinjaAPI(version='0.1.0')
 
 
-@api.get("/hello")
-def hello(request):
-    return "Hello world"
+@api.get("/list/{series_id}")
+def get_url_list():
+    pass
+
+
+@api.get("/upload")
+def get_upload_session():
+    pass
+
+
+@api.get("/search")
+def keyword_search():
+    pass
+
+
+@api.post("/athorize")
+def user_athorizz():
+    pass
