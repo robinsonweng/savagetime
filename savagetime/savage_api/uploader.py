a7a1986dc4a2b665b72d874d02eefc6026b6f713
robinsonweng <robinsonweng100@gmail.com>
2021-12-20 17:43:55 +0800

    Init Typing class

--- savagetime/savage_api/uploader.py
+++ savagetime/savage_api/uploader.py
@@ -6,3 +6,6 @@ from django.conf import settings
 from savage_api.exceptions import (
     InvalidHeader, InvalidQuery
 )
+
+Chunk = TypeVar('Chunk', bound="FileChunk")
+Uploader = TypeVar('Uploader', bound="UploaderFile")
