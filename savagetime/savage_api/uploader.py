1364ee773f43fe9b6948a368a1d9399cf7b945c6
robinsonweng <robinsonweng100@gmail.com>
2021-12-20 17:35:04 +0800

    Import libraries
    Import typing for type check.
    Import HttpError for custom exception to inherit
    Import cache to access django cache
    Import HttpResponse for type check
    Import settings to access django setting
    Import InvalidHeader, InvalidQuery to raise error

---
+++ savagetime/savage_api/uploader.py
@@ -0,0 +1,8 @@
+from typing import TypeVar, Type, List
+from ninja.errors import HttpError
+from django.core.cache import caches
+from django.http import HttpResponse
+from django.conf import settings
+from savage_api.exceptions import (
+    InvalidHeader, InvalidQuery
+)
