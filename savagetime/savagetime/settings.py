87a14ff5a6fb005fbc679651ff44d9c5b0dd0df8
robinsonweng <robinsonweng100@gmail.com>
2021-12-20 15:15:25 +0800

    Use yaml as setting file
    Because the .env file didn't support dict or list structure,
    I decide changing the setting file using yaml

--- savagetime/savagetime/settings.py
+++ savagetime/savagetime/settings.py
@@ -10,11 +10,14 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import yaml
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+with open(BASE_DIR.parent / "dev_setting.yaml", "r") as f:
+    setting = yaml.load(f, yaml.SafeLoader)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
