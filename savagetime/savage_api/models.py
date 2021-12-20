35f9e3e97646bfb95fcd1fb44e5861351d310a68
robinsonweng <robinsonweng100@gmail.com>
2021-12-20 17:25:06 +0800

    Update comment in models.py

--- savagetime/savage_api/models.py
+++ savagetime/savage_api/models.py
@@ -3,12 +3,12 @@ from django.db import models
 # Create your models here.
 
 from django.utils import timezone
-import uuid  # TODO: use admin user id for salt
+import uuid  # TODO: What if uuid repeat?
 
 
 class Series(models.Model):
     uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    name = models.TextField(blank=True)  # TODO: force uppercase before commit to db
+    name = models.TextField(blank=True)
     season = models.CharField(max_length=20)
     episodes = models.IntegerField(blank=True)
     pub_year = models.CharField(max_length=20)
@@ -34,15 +34,14 @@ class Video(models.Model):
             The rest or it like HunterXHunter(new ver.)have total 148 episode,
             Fate/stay Night[Heaven's Feel] have 3 episode but 2h each. These two extrems
             may cause imbalance tree in linux FS(verificaiton required)
-            update: these two extreams will cause imbalance directory tree, and
+        update:
+            these two extremes will cause imbalance directory tree and
             Inode table search inefficient in linx FS, but the solution is not long-term.
             It would be a better idea to mount spacific linux FS for large file storage
         """
-        # only avalible for admin
         exet = filename.split('.')[1]
         return "{0}/{1}.{2}".format(instance.series_id, instance.uuid, exet)
-    # filefield is not path!
-    file_field = models.FileField(upload_to=filedir_path)  # TODO:  should be array
+    file_field = models.FileField(upload_to=filedir_path)
 
     def __str__(self) -> str:
         return f"{self.series.name}[{self.episode}]"
