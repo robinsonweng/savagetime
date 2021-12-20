b84253165e6a8bddcb8a6c48d9cc936cccb2d116
robinsonweng <robinsonweng100@gmail.com>
2021-12-20 17:44:24 +0800

    Init class FileChunk
    This clss represent same chunk as client send, if require header
    is missing, raise an error(http 400)

--- savagetime/savage_api/uploader.py
+++ savagetime/savage_api/uploader.py
@@ -9,3 +9,12 @@ from savage_api.exceptions import (
 
 Chunk = TypeVar('Chunk', bound="FileChunk")
 Uploader = TypeVar('Uploader', bound="UploaderFile")
+
+
+class FileChunk(object):
+    def __init__(self, request) -> None:
+        self.chunk_length = request.headers.get("Content-Length")
+        self.chunk_type = request.headers.get("Content-Type")
+        self.chunk_range = request.headers.get("Content-Range")
+        self.chunk = request.body
+        self.check_headers()
