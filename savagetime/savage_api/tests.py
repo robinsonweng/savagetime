85b1975df0040d77eb88f56e09da04328cdd0c95
robinsonweng <robinsonweng100@gmail.com>
2022-01-02 17:15:56 +0800

    Import necessary modules

--- savagetime/savage_api/tests.py
+++ savagetime/savage_api/tests.py
@@ -1,3 +1,9 @@
+# Test for app savage_api
+import os
+import json
+import base64
 from django.test import TestCase
+from django.test import Client
+from savage_api.models import Series
 
 # Create your tests here.
