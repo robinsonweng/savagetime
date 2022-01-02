6df712948624ed43123fc21dcbff72436de5746c
robinsonweng <robinsonweng100@gmail.com>
2022-01-02 16:28:22 +0800

    Add default value to parameter in class initializer

--- savagetime/savage_api/exceptions.py
+++ savagetime/savage_api/exceptions.py
@@ -3,13 +3,13 @@ from ninja.errors import HttpError
 
 
 class InvalidHeader(HttpError):
-    def __init__(self, status, message):
+    def __init__(self, status, message=""):
         self.status = status
         self.message = message
 
 
 class InvalidQuery(HttpError):
-    def __init__(self, status, message):
+    def __init__(self, status, message=""):
         self.status = status
         self.message = message
 
