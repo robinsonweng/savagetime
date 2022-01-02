fd51238d9f0b02b93fb798cb76169cbf8b7798c7
robinsonweng <robinsonweng100@gmail.com>
2022-01-02 17:16:40 +0800

    Add unit test setup

--- savagetime/savage_api/tests.py
+++ savagetime/savage_api/tests.py
@@ -6,4 +6,20 @@ from django.test import TestCase
 from django.test import Client
 from savage_api.models import Series
 
-# Create your tests here.
+
+class CreateVideoMetadataTest(TestCase):
+    def setUp(self):
+        self.api = "/api/dev/"
+        self.video_route = os.path.join(self.api, 'video') 
+        self.upload_url = ""
+        self.request = Client()
+        Series.objects.create(
+            name="月光下的異世界之旅",
+            season="spring",
+            episodes=12,
+            pub_year="2021",
+            pub_month="1",
+            finale=True,
+            subtitle_group="",
+        )
+
