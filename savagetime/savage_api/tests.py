04f1d35a94f627987b2083e33bd9f419e852968c
robinsonweng <robinsonweng100@gmail.com>
2022-01-02 17:20:48 +0800

    Add test case for route video in put method

--- savagetime/savage_api/tests.py
+++ savagetime/savage_api/tests.py
@@ -37,3 +37,19 @@ class CreateVideoMetadataTest(TestCase):
         )
         self.upload_url = json.loads(response.content.decode('utf-8'))["upload_url"]
         self.assertEqual(200, response.status_code)
+
+    def test_video_put_wrong_upload_id(self):
+        fake_id = ""
+        salt = os.urandom(5)
+        fake_id = base64.b64encode(salt).decode()
+        upload_url = f'{self.video_route}?upload_id={fake_id}'
+        response = self.request.put(
+            upload_url, data=None, content_type='video/*'
+        )
+        self.assertEqual(400, response.status_code, response.content)
+        error_msg = {
+            "message": "Unexpected Query",
+            "detail": "id not found or session expire"
+        }
+        rsp_msg = response.content.decode('utf-8')
+        self.assertDictEqual(error_msg, json.loads(rsp_msg), rsp_msg)
