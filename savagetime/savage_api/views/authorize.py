309d2493835589d42503ee2198390a03bfbfeb2b
robinsonweng <robinsonweng100@gmail.com>
2022-01-06 13:28:41 +0800

    Add empty view function

--- savagetime/savage_api/views/authorize.py
+++ savagetime/savage_api/views/authorize.py
@@ -5,3 +5,13 @@ from ninja import Router
 
 authorize_router = Router()
 authorize_router.prefix = "authorize/"
+
+
+@authorize_router.api_operation(["GET"], "/")
+def get_authorize():
+    pass
+
+
+@authorize_router.api_operation(["POST"], "/")
+def post_authroize():
+    pass
