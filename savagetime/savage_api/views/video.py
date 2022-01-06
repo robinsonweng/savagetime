cfc0248068fa7a2c08a3e32ce87842f044ae7849
robinsonweng <robinsonweng100@gmail.com>
2022-01-06 13:34:18 +0800

    Initialize video router

---
+++ savagetime/savage_api/views/video.py
@@ -0,0 +1,31 @@
+# view functions for video route
+
+import os
+import base64
+
+from django.urls import reverse
+
+from ninja import Router
+from ninja.constants import NOT_SET
+
+from ..model.models import (
+    Video, Series
+)
+from ..model.schemas import (
+    Videoget, Videoin
+)
+
+from ..responses.exceptions import (
+    InvalidQuery, InvalidHeader, UnexpetedRequest
+)
+from ..responses.response import (
+    UploadStatusResponse
+)
+
+from ..utils.uploader import Uploader, FileChunk
+
+
+no_response_body_set = frozenset({201, 204, 308})
+
+video_router = Router()
+video_router.prefix = "video/"  # set the root route
