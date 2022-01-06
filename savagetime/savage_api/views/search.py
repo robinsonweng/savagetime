48b6227d93d37882cc799dd0f1c83a36933f0cec
robinsonweng <robinsonweng100@gmail.com>
2022-01-06 13:29:58 +0800

    Add empty view function

--- savagetime/savage_api/views/search.py
+++ savagetime/savage_api/views/search.py
@@ -5,3 +5,8 @@ from ninja import Router
 
 search_router = Router()
 search_router.prefix = "search/"
+
+
+@search_router.api_operation(["GET"], "/")
+def get_search_result(request, keyword: str):
+    pass
