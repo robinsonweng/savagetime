f00a064796525dac37a3f56d20f8e27e535ec8eb
robinsonweng <robinsonweng100@gmail.com>
2022-01-06 20:23:40 +0800

    Rewrite method in ninja.Route
    Create a middleware by rewrite the method add_api_operation.
    To do that, I subclass the original class Route, then since my goal
    isn't change how parent function behave, we rewrite the method, but
    return the parent function and pass all paramater to it. So the method
    can do new things when called, but still remain it's own job.
    Then,I wrap up the paramater view function with other function
    that need to execute in spacific timing and also pass the same paramter
    that view function require. Finally we pass the view function back to
    parent function as paramater. So everytime when django runs a view
    function, it also provoke our middleware(before request & after request)

--- savagetime/savage_api/libs/route.py
+++ savagetime/savage_api/libs/route.py
@@ -1,6 +1,49 @@
+from typing import List, Callable
+from functools import wraps
+
 from ninja import Router
 
 
 class SavageRouter(Router):
     def __init__(self, *args, **kwrags):
+        self._before_request_queue: Callable = []
+        self._after_request_queue: Callable = []
         super().__init__(*args, **kwrags)
+
+    def add_api_operation(
+        self,
+        path: str,
+        methods: List[str],
+        view_func: Callable,
+        *args,
+        **kwargs
+    ) -> Callable:
+        def decorator(view_func: Callable) -> Callable:
+            # ^ this function don't do decorator stuff, just a wrapper
+            @wraps(view_func)
+            def wrap(request, *args, **kwargs):
+                # before request
+                if self._before_request_queue is not None:
+                    for before_request_func in self._before_request_queue:
+                        before_request_func(request, *args, **kwargs)
+                response = view_func(request, *args, **kwargs)
+                if self._after_request_queue is not None:
+                    for after_request_func in self._after_request_queue:
+                        after_request_func(request, *args, **kwargs)
+                # after request
+                return response
+            return wrap
+        view_func = decorator(view_func)
+        return super().add_api_operation(path, methods, view_func, *args, **kwargs)
+
+    def before_reqeust(self):
+        def decorator(func: Callable) -> Callable:
+            self._before_request_queue.append(func)
+            return func
+        return decorator
+
+    def after_request(self):
+        def decorator(func: Callable) -> Callable:
+            self._after_request_queue.append(func)
+            return func
+        return decorator
