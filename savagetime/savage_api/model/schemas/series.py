97ef9b120941c646e94726987030b6f4aebadf04
robinsonweng <robinsonweng100@gmail.com>
2022-01-11 14:51:49 +0800

    Add attribute check in series schema

--- savagetime/savage_api/model/schemas/series.py
+++ savagetime/savage_api/model/schemas/series.py
@@ -5,6 +5,22 @@ from ..models import Video, Series
 from django.utils import timezone
 
 
+class SeriesInputBase(Schema):
+    def __setattr__(self, name, value):
+        if name == "season":
+            season_zhtw = ["春", "夏", "秋", "冬"]
+            if value not in season_zhtw:
+                raise AttributeError(f"value: '{value}' is not a member of {season_zhtw}")
+        elif name == "pub_year":
+            current_year = timezone.now().year
+            year = [y for y in range(1999, current_year + 1)]
+            if value not in year:
+                raise AttributeError(f"value: '{value}' is not a member of {year}")
+        elif name == "pub_month":
+            month = [m for m in range(1, 12 + 1)]
+            if value not in month:
+                raise AttributeError(f"value: '{value}' is not a member of {month}")
+        return super().__setattr__(name, value)
 
 
 class SeriesInfoOutput(Schema):
