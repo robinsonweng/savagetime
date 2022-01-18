d68c73b6530c8fbc44a7ab96a377bd6c6384718f
robinsonweng <robinsonweng100@gmail.com>
2022-01-18 13:44:47 +0800

    Fix wrong url formatter

--- savagetime/savage_api/views/video.py
+++ savagetime/savage_api/views/video.py
@@ -93,7 +93,7 @@ def create_video_metadata(request, metadata: VideoUploadPostInput):
 
     return {
         "status": "200",
-        "upload_url": f"{reverse('api-dev:post_upload_video')}?upload_id={upload_id}"
+        "upload_url": f"{reverse('api-dev:video_upload')}?upload_id={upload_id}"
     }
 
 
