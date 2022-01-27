3ccfcfdf16c2f8fb56c9eced325a18144324ecd0
robinsonweng <robinsonweng100@gmail.com>
2022-01-27 13:50:06 +0800

    Add: script for running nginx

---
+++ scripts/nginx_reload.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cd /usr/local/nginx/sbin
+sudo ./nginx -s reload -c conf/savage_nginx_conf/nginx.conf
---
+++ scripts/nginx_start.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cd /usr/local/nginx/sbin
+sudo ./nginx -c conf/savage_nginx_conf/nginx.conf
