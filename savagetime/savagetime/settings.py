d8f593cae7982fce4a423d97b410dfc11ec2c125
robinsonweng <robinsonweng100@gmail.com>
2022-01-27 14:19:02 +0800

    Modify: just use const to install app savage_api

--- savagetime/savagetime/settings.py
+++ savagetime/savagetime/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    *setting["INSTALLED_APPS"]
+    'savage_api',
 ]
 
 MIDDLEWARE = [
