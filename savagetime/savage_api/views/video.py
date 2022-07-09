9c050161d13fcf204c3f5cc535ee4f7b227702a4
robinsonweng <robinsonweng100@gmail.com>
2022-07-09 08:03:25 +0800

    Add fake stream url info

--- savagetime/savage_api/views/video.py
+++ savagetime/savage_api/views/video.py
@@ -47,22 +47,11 @@ uploader_cache = caches[settings.RESUMEABLE_UPLOADER_CACHE_CONFIG]
 @video_router.api_operation(
     ["GET"],
     "/{video_id}/stream",
-    response={200: dict},
-    url_name="video_stream",
-    auth=None,
+    response=NOT_SET,
+    url_name="video_stream"
 )
 def get_video_stream(request, video_id: str):
-    ext = "m3u8"
-    cdn = "127.0.0.2"
-    base_stream = "index"
-    md5 = "id2tPFIcl59pAG1kcorMig"
-    # check if expire out of range
-    # verify md5
-    # md5 $path$ext
-    # secure_link $md5$expire salt
-    # echo -n '2147483647/s/link127.0.0.1 secret' | \
-    # openssl md5 -binary | openssl base64 | tr +/ -_ | tr -d =
-    return {"url": f"{cdn}/hls/123.mp4/{base_stream}.{ext}?md5={md5}"}
+    pass
 
 
 """
